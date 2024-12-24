dae0e8f24154c1903653fbe91dde72e346e80256
taylorpaisie <tpaisie91@gmail.com>
2024-12-24 12:20:40 -0500

    making first MSA viewer for snp wf dash app

---
+++ app.py
@@ -0,0 +1,43 @@
+import base64
+from dash import Dash, html, dcc, Input, Output, State
+import dash_bio as dashbio
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ', html.A('Select a FASTA File')
+        ]),
+        style={
+            'width': '100%', 'height': '60px', 'lineHeight': '60px',
+            'borderWidth': '1px', 'borderStyle': 'dashed', 'borderRadius': '5px',
+            'textAlign': 'center', 'margin': '10px'
+        },
+        multiple=False
+    ),
+    html.Div(id='output-alignment-chart')
+])
+
+
+@app.callback(
+    Output('output-alignment-chart', 'children'),
+    Input('upload-data', 'contents'),
+    State('upload-data', 'filename')
+)
+def update_output(file_contents, file_name):
+    if file_contents is not None and file_name.endswith('.fasta'):
+        content_type, content_string = file_contents.split(',')
+        decoded = base64.b64decode(content_string).decode('utf-8')
+
+        return dashbio.AlignmentChart(
+            id='alignment-viewer',
+            data=decoded
+        )
+    else:
+        return html.Div("Please upload a valid FASTA file.")
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
